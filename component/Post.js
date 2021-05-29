59372bf4312e92200e89864e609320bb6b2d88e4
0JIEUN0 <jieun4436@naver.com>
2021-05-29 16:07:18 +0900

    Small changes

--- component/Post.js
+++ component/Post.js
@@ -24,14 +24,6 @@ function Post ( props ) {
 
             {/* 이미지 */}
             <CardItem>
-            <PagerView style={styles.pagerView} initialPage={0}>
-      <View key="1">
-        <Text>First page</Text>
-      </View>
-      <View key="2">
-        <Text>Second page</Text>
-      </View>
-    </PagerView>
                 <PagerView style={styles.pagerView} initialPage={0}>
                     {data.pic.map(img => 
                     <Image
